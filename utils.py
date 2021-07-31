b1977c9d7b263eadc687d35e420cd752d9dd634b
Lino <49255601+Its-Lino@users.noreply.github.com>
2021-07-31 04:44:34 -0400

    Update utils.py
    Add GetCogs()

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
+""" Utility Module designed to be the main library for HangoutCore """
+
 import logging
 import shutil
 import discord
-import colorama
-from discord import utils
+import os
 import config
 import sys
-import youtube_dl as ytdl
+import traceback
 
 from colorama import *
 from os import system, name
@@ -13,12 +14,65 @@ from os import system, name
 cfg = config.load_config()
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# ↓ Color ↓
+# ↓ Cog Loading ↓ : WIP
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-def color(style: colorama.Style, fore: colorama.Fore, back: colorama.Back, content):
-    processedContent = f"{style}{fore}{back}{content}{Style.RESET_ALL}{Fore.RESET}{Back.RESET}"
-    return processedContent
+def GetCogs():
+    valid_files = []
+    disabled_files = []
+    invalid_files = []
+    for file in os.listdir(config.COG_DIRECTORY_PATH):
+        if file.endswith('.py'):
+            valid_files.append(file)
+        elif file.endswith('.disabled'):
+            disabled_files.append(file)
+        else:
+            if file == "__pycache__" or "__init__":
+                pass
+            else:
+                invalid_files.append(file)
+    package = {
+        "valid_files": valid_files,
+        "invalid_files": invalid_files,
+        "disabled_files": disabled_files
+    }
+    return package
+    
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# ↓ Error Processing ↓ : WIP
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+class ErrorProcessing():
+
+    def CogLoadError(file, error, debug_mode):
+        """ Error Processing tailored to display the specific error when loading a cog, 
+            designed to minimise clutter and lines not relevant."""
+        ErrorTraceback = traceback.format_exception(type(error), error, error.__traceback__)
+        if debug_mode:
+            log("ERROR", "\n" + "".join(ErrorTraceback))
+        else:
+            FilteredTraceback = None
+            for string in ErrorTraceback:
+                str(string).replace("\n", "")
+            log("ERROR", f"""  └ Failed to load {file}.
+            {Back.RED}{ErrorTraceback[0]}{Back.RESET}
+            {Back.RED}{ErrorTraceback[5]}{Back.RESET}
+            {Back.RED}{ErrorTraceback[6]}{Back.RESET}""")
+            #log("ERROR", f"{Back.RED}    {ErrorTraceback[0]}{Back.RESET}")
+            #log("ERROR", f"{Back.RED}    {ErrorTraceback[5]}{Back.RESET}")
+            #log("ERROR", f"{Back.RED}    {ErrorTraceback[6]}{Back.RESET}")
+            
+            #for i in range(len(ErrorTraceback)):
+            #    log("ERROR", f"{Fore.RED}    {ErrorTraceback[i]}{Fore.RESET}")
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# ↓ Color ↓ : WIP
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+#def color(style: colorama.Style, fore: colorama.Fore, back: colorama.Back, content):
+#    processedContent = f"{style}{fore}{back}{content}{Style.RESET_ALL}{Fore.RESET}{Back.RESET}"
+#    return processedContent
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # ↓ Logging ↓
@@ -100,3 +154,12 @@ class terminal:
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # ↓ FFMPEG and Youtube_DL Stuff ↓
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+class GuildState:
+    """Helper class managing per-guild state."""
+
+    def __init__(self):
+        self.volume = 1.0 # volume is config max_volume / 100
+        self.playlist = []
+        self.skip_votes = set()
+        self.now_playing = None
