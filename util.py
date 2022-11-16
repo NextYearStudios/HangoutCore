d0067ae4adc19300e01fa0cca67babaa954267c5
Lino H <hutchinsl0507@gmail.com>
2022-11-16 04:22:06 +0000

    update util.py

--- util.py
+++ util.py
@@ -147,21 +147,25 @@ class bot():
                     await user.add_roles(role, reason="User added via AutoRole.")
                     await interaction.response.send_message(f"You've successfully been assigned {role.mention}.", ephemeral=True)
 
-        class confirmationView(discord.ui.View):
+        class Confirm(discord.ui.View):
             def __init__(self):
-                super().__init__(timeout=3000)
+                super().__init__()
                 self.value = None
 
+            # When the confirm button is pressed, set the inner value to `True` and
+            # stop the View from listening to more input.
+            # We also send the user an ephemeral message that we're confirming their choice.
             @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
-            async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+            async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+                await interaction.response.send_message('Confirming', ephemeral=True)
                 self.value = True
-                await interaction.response.send_message(f'Removing..', ephemeral=True)
                 self.stop()
 
+            # This one is similar to the confirmation button except sets the inner value to `False`
             @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
-            async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+            async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+                await interaction.response.send_message('Cancelling', ephemeral=True)
                 self.value = False
-                await interaction.response.send_message(f'Cancelled.', ephemeral=True)
                 self.stop()
 
     class CustomButtons():
