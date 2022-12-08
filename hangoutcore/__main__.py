c5732e1b9a193dcdb8b0e73cf63fd99776701d87
Lino <49255601+Its-Lino@users.noreply.github.com>
2022-12-08 06:52:17 +0000

    update

--- hangoutcore/__main__.py
+++ hangoutcore/__main__.py
@@ -11,19 +11,57 @@
         None
     ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 """
+import anyio
 import asyncio
+import click
 import sys
 
 from hangoutcore.util import *
 from hangoutcore.bot import HangoutCoreBot
 
+# @click.command()
+# @click.option('--debug','-d', default=False, help="Enable bot debug mode.")
+# @click.option('--config','-c', default=None, help="Specify which config the bot should use.")
+# @click.option('--token','-t', default=-1, help="Specify which token the bot should use from the config.")
 async def main():
-    print(sys.argv)
+    debug = False
+    config = None
+    token = -1
+    if '-h' in sys.argv[1:] or '--help' in sys.argv[1:]:
+        print(f"""
 
-async def init():
+    ██╗░░██╗░█████╗░███╗░░██╗░██████╗░░█████╗░██╗░░░██╗████████╗░█████╗░░█████╗░██████╗░███████╗
+    ██║░░██║██╔══██╗████╗░██║██╔════╝░██╔══██╗██║░░░██║╚══██╔══╝██╔══██╗██╔══██╗██╔══██╗██╔════╝
+    ███████║███████║██╔██╗██║██║░░██╗░██║░░██║██║░░░██║░░░██║░░░██║░░╚═╝██║░░██║██████╔╝█████╗░░
+    ██╔══██║██╔══██║██║╚████║██║░░╚██╗██║░░██║██║░░░██║░░░██║░░░██║░░██╗██║░░██║██╔══██╗██╔══╝░░
+    ██║░░██║██║░░██║██║░╚███║╚██████╔╝╚█████╔╝╚██████╔╝░░░██║░░░╚█████╔╝╚█████╔╝██║░░██║███████╗
+    ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░░╚════╝░░╚═════╝░░░░╚═╝░░░░╚════╝░░╚════╝░╚═╝░░╚═╝╚══════╝
+    ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+[-c, --config : string  ] Load the specified config, Also sets as default. If the specified config cannot be found, return to previous config as a precaution.
+[-d, --debug  : bool    ] Enable debug mode for more information to be displayed in the terminal. Changes log mode to DEBUG.
+[-n, --new    : empty   ] Manually put HangoutCore in a setup state in order to create a new configuration. Sets the new config as default.
+[-s, --silent : bool    ] Enable/Disable to have information sent to log only, or terminal and log(). Useful for running as a service since there's no access to terminal.
+[-t, --token  : integer ] Specify which token to use if you're using multiple. Allows user to skip token choice prompt.
+        """)
+        sys.exit(0)
+    if len(sys.argv[1:]) >= 2:
+        pass
+    print(sys.argv[1:])
+    print(len(sys.argv[1:]))
+    print('test')
+
+
+def init():
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
         Terminal().Log().CRITICAL(f"Please refrain from using CTRL+C to shutdown bot.")
         Terminal().Log().CRITICAL(f"Shutting Down...")
         sys.exit(0)
+    except:
+        Terminal().Log().CRITICAL(f"Shutting Down...")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    init()
