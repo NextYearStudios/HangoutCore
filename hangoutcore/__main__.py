fba5b3ec6e794e7d89aab2ff6d72c5553bd6654d
Lino <49255601+Its-Lino@users.noreply.github.com>
2022-12-08 08:22:22 +0000

    Begin argument support.

--- hangoutcore/__main__.py
+++ hangoutcore/__main__.py
@@ -19,15 +19,13 @@ import sys
 from hangoutcore.util import *
 from hangoutcore.bot import HangoutCoreBot
 
-# @click.command()
-# @click.option('--debug','-d', default=False, help="Enable bot debug mode.")
-# @click.option('--config','-c', default=None, help="Specify which config the bot should use.")
-# @click.option('--token','-t', default=-1, help="Specify which token the bot should use from the config.")
 async def main():
+    argv = sys.argv[1:]
     debug = False
     config = None
     token = -1
-    if '-h' in sys.argv[1:] or '--help' in sys.argv[1:]:
+
+    if '-h' in sys.argv or '--help' in argv:
         print(f"""
 
     ██╗░░██╗░█████╗░███╗░░██╗░██████╗░░█████╗░██╗░░░██╗████████╗░█████╗░░█████╗░██████╗░███████╗
@@ -43,13 +41,55 @@ async def main():
 [-n, --new    : empty   ] Manually put HangoutCore in a setup state in order to create a new configuration. Sets the new config as default.
 [-s, --silent : bool    ] Enable/Disable to have information sent to log only, or terminal and log(). Useful for running as a service since there's no access to terminal.
 [-t, --token  : integer ] Specify which token to use if you're using multiple. Allows user to skip token choice prompt.
-        """)
+        """) # Formatting looks ugly I know. Looks pretty good in the terminal though...
         sys.exit(0)
-    if len(sys.argv[1:]) >= 2:
-        pass
-    print(sys.argv[1:])
-    print(len(sys.argv[1:]))
-    print('test')
+
+    if len(argv) >= 2:
+        if '-c' in argv:
+            argvPos = argv.index('-c')
+            config = argv[argvPos + 1]
+            argv.pop(argvPos + 1)
+            argv.pop(argvPos)
+        elif '--config' in argv:
+            argvPos = argv.index('--config')
+            config = argv[argvPos + 1]
+            argv.pop(argvPos + 1)
+            argv.pop(argvPos)
+        
+        # Final check to make sure there's no invalid arguments that we missed.
+        if len(argv) > 0:
+            Terminal().Log().CRITICAL(f"There appears to be invalid arguments in your entry. Please Double check your spelling and try again.\nYour input: {' '.join(sys.argv)}\nInvalid argument(s): {' '.join(argv)}")
+            sys.exit(0)
+        
+    print(debug)
+    print(config)
+    print(token)
+
+    exampleToken = "1234567890.1234567890.1234567890"
+
+    # This function will be moved to util
+    def obfuscateString(inputString:str, amount:int=4, obfuscateChar:str='#'):
+        """
+        Used for allowing our user to recognize a string/token while keeping the rest of it hidden from prying eyes such as a twitch stream, a screen recording or any other public environment.
+        """
+
+        outputString = ""
+        for i in range(len(inputString)):
+            if i < len(inputString) - amount:
+                if inputString[i] == '.':
+                    outputString = outputString + inputString[i]
+                else:
+                    outputString = outputString + obfuscateChar
+            elif i == len(inputString) - amount:
+                outputString = outputString + "-" + inputString[i]
+            else:
+                outputString = outputString + inputString[i]
+        return outputString
+
+    obToken = obfuscateString(exampleToken, 4, '*')
+    
+    print(obToken)
+    
 
 
 def init():
@@ -59,9 +99,9 @@ def init():
         Terminal().Log().CRITICAL(f"Please refrain from using CTRL+C to shutdown bot.")
         Terminal().Log().CRITICAL(f"Shutting Down...")
         sys.exit(0)
-    except:
-        Terminal().Log().CRITICAL(f"Shutting Down...")
-        sys.exit(0)
+    # except:
+    #     Terminal().Log().CRITICAL(f"Shutting Down...")
+    #     sys.exit(0)
 
 if __name__ == "__main__":
     init()
