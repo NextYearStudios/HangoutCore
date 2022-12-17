fe148407412334fcfb233048e993bbbb4f80bce9
Lino <hutchinsl0507@gmail.com>
2022-12-16 19:17:53 -0500

    Update: Update to match util async functions

--- hangoutcore/bot.py
+++ hangoutcore/bot.py
@@ -14,7 +14,7 @@
 
 import discord
 
-from hangoutcore.util import Audio, Config, Local, Terminal
+from hangoutcore.util import Audio, Config, Database, Local, Terminal
 from aiohttp import ClientSession
 from datetime import datetime
 from discord.ext import commands
@@ -24,6 +24,7 @@ class HangoutCoreBot(commands.Bot):  # Sub class bot so we can have more customi
     def __init__(
             self,
             *args,
+            activity,
             test_Guild_ID: Optional[int] = None,
             web_client,
             debug_mode: Optional[bool] = False,
@@ -35,53 +36,69 @@ class HangoutCoreBot(commands.Bot):  # Sub class bot so we can have more customi
         # Setup variables that we may need later on
 
         self.start_time = None  # Add the start_time variable so that we may access it for debugging or information display purposes.
-        self.test_Guild_ID : int = test_Guild_ID # This is for testing slash commands. That way we don't sync globally every time we run
         self.debug_mode = debug_mode
 
         self.web_client = web_client
         self.config = config
         self.terminal = terminal
+        self.log = terminal.Log()
+
+        self.audio = Audio()
+        self.database = Database()
+        self.local = Local()
+        self.activity = activity
 
 
         self.BotSynced = False
         # BotSynced = False
         # ViewsAdded = False
-        self.local = Local()
-        self.local.setConfig(self.config)
-        self.local.setTerminal(self.terminal)
 
     async def setup_hook(self) -> None:
 
         # here, we are loading extensions prior to sync to ensure we are syncing interactions defined in those extensions.
         self.start_time = '{0:%d%b%Y %Hh:%Mm}'.format(datetime.now())
-        self.terminal.Log().INFO(f"Checking for preloaded bot modules...")
+
+        
+        await self.audio.setConfig(self.config)
+        await self.audio.setTerminal(self.terminal)
+        await self.audio.setConfig(self.config)
+        await self.audio.setTerminal(self.terminal)
+        await self.local.setConfig(self.config)
+        await self.local.setTerminal(self.terminal)
+
+        await self.log.INFO(f"Checking for preloaded bot modules...")
 
         for cog in self.cogs.keys():
-            self.terminal.Log().INFO(f"Found {cog}")
+            await self.log.INFO(f"Found {cog}")
 
-        self.terminal.Log().INFO(f"Looking for bot modules in '/{self.config.COG_DIRECTORY_PATH}'...")
+        await self.log.INFO(f"Looking for bot modules in '/{self.config.COG_DIRECTORY_PATH}'...")
         await self.local.load_extensions(self, self.debug_mode)  # Scan cog directory and enable cogs.
 
-        Audio().verify_opus()  # Looks for opus and loads it if found.
+        await self.audio.verify_opus()  # Looks for opus and loads it if found.
 
-        if self.debug_mode or self.test_Guild_ID is not None:
-            self.tree.sync(guild=self.test_Guild_ID)
+        if self.debug_mode:
+            devGuildID = self.config.CONFIG['bot']['developer_guild_id']
+            if int(devGuildID) != 0:
+                await self.log.DEBUG(f"Syncing to guild ID: {devGuildID}")
+                await self.tree.sync(guild=discord.Object(devGuildID))
+            else:
+                await self.log.ERROR(f"Unable to sync to developer guild. Please provide your Guild ID in your config file.")
         else:
             if not self.BotSynced:
                 await self.tree.sync()
                 self.BotSynced = True
         
 
-        self.terminal.Log().INFO(f"Logged in as {self.user} (ID: {self.user.id}).")
+        await self.log.INFO(f"Logged in as {self.user} (ID: {self.user.id}).")
 
     async def on_ready(self):
         # self.add_view(utils.bot.CustomViews.autoroleView())
         await self.wait_until_ready()
         
-        self.terminal.print_hr()
-        self.terminal.print_center("Bot Is Online")
-        self.terminal.print_hr()
-        self.terminal.Log().WARNING("Updating Guild Database")
+        await self.terminal.print_hr()
+        await self.terminal.print_center("Bot Is Online")
+        await self.terminal.print_hr()
+        await self.log.WARNING("Updating Guild Database")
         for guild in self.guilds:
             # await bot.database.RegisterGuild(guild)
-            self.terminal.Log().INFO(f"Registered {guild.name}:{guild.id}")
+            await self.log.INFO(f"Registered {guild.name}:{guild.id}")
