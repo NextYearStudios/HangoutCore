e13f316f2f5d27416e6c45d1f520d39e6497e817
Lino <49255601+Its-Lino@users.noreply.github.com>
2024-12-22 01:03:44 -0800

    v4.0.0 base

--- hangoutcore/_version.py
+++ hangoutcore/_version.py
@@ -3,7 +3,6 @@
 TYPE_CHECKING = False
 if TYPE_CHECKING:
     from typing import Tuple, Union
-
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
     VERSION_TUPLE = object
@@ -13,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "4.0.1.dev0+ga6d7182.d20241222"
-__version_tuple__ = version_tuple = (4, 0, 1, "dev0", "ga6d7182.d20241222")
+__version__ = version = '4.0.1.dev3+g7b2325d.d20241222'
+__version_tuple__ = version_tuple = (4, 0, 1, 'dev3', 'g7b2325d.d20241222')
