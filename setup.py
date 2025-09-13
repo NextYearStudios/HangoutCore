8087f2b5f0f3751cedc52cd09e26b9398ad66f0e
Lino <49255601+Its-Lino@users.noreply.github.com>
2025-09-13 14:57:54 -0700

    Reset code in prep for new project

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_namespace_packages, find_packages, setup
 
-if __name__ == '__main__':
-    setup()
+if __name__ == "__main__":
+    setup(packages=find_packages())
